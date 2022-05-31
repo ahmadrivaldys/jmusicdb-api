44c72da11e697953e0022b518c9bab132534a5ef
ahmadrivaldys <ahmadrivaldys@gmail.com>
2022-05-31 09:03:37 +0700

    Refactoring authentication

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,4 +1,115 @@
-const admin = require('./admin')
-const user  = require('./user')
+// Importing modules
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { v4: uuidv4 } = require('uuid')
+const { validationResult } = require('express-validator')
 
-module.exports = { admin, user }
+// Importing models
+const AccountType = require('../../models/account_type')
+const Admin = require('../../models/admin')
+const User = require('../../models/user')
+
+const register = async (req, res, next) =>
+{
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty())
+    {
+        return res.status(422).json({ errors: errors.mapped() })
+    }
+    
+    try
+    {
+        const { username, fullname, email, password, account_type_code } = req.body
+        const uuid = uuidv4()
+
+        const salt = bcrypt.genSaltSync(10)
+        const passwordHash = bcrypt.hashSync(password, salt)
+        const accountType = await AccountType.query()
+            .where('code', account_type_code)
+            .select('id')
+            .first()
+
+        await User.query()
+            .insert({
+                uuid,
+                username,
+                fullname,
+                email,
+                password: passwordHash,
+                account_type_id: accountType.id
+            })
+
+        return res.status(201)
+            .json({
+                statusCode: 201,
+                statusText: 'Created',
+                message: 'Successfully created user account.'
+            })
+    }
+    catch(error)
+    {
+        console.log(error)
+        next(error)
+    }
+}
+
+const login = async (req, res) =>
+{
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty())
+    {
+        return res.status(422).json({ errors: errors.mapped() })
+    }
+    
+    try
+    {
+        const getAccount = () =>
+        {
+            if(req.params.account_type === 'admin')
+            {
+                const getAdminAccount = await Admin.query()
+                    .where('username', req.body.username)
+                    .first()
+                
+                return getAdminAccount
+            }
+            
+            const getUserAccount = await User.query()
+                .where('username', req.body.username)
+                .first()
+
+            return getUserAccount
+        }
+
+        const generateToken = jwt.sign({
+                uuid: getAccount.uuid,
+                username: getAccount.username
+            },
+            process.env.JWT_SECRET_KEY,
+            {
+                expiresIn: '7d'
+            })
+
+        return res.status(200)
+            .json({
+                statusCode: 200,
+                statusMessage: 'OK',
+                message: 'Log in successful.',
+                token: generateToken
+            })
+    }
+    catch(error)
+    {
+        console.log(error)
+        next(error)
+    }
+}
+
+const logout = (req, res, next) =>
+{
+    // 
+}
+
+module.exports = { register, login, logout }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,9 @@
-const router   = require('express').Router()
+const router = require('express').Router()
 const validate = require('../middlewares/validate-input')
-const { admin: authAdmin, user: authUser } = require('../controllers/auth')
+const auth = require('../controllers/auth')
 
-router.post('/auth/admin/login', validate.admin.login, authAdmin.login)
-router.post('/auth/admin/logout', authAdmin.logout)
-router.post('/auth/register', validate.auth.register, authUser.register)
-router.post('/auth/login', validate.auth.login, authUser.login)
-router.post('/auth/logout', authUser.logout)
+router.post('/auth/register', validate.auth.register, auth.register)
+router.post('/auth/login/:account_type', validate.auth.login, auth.login)
+router.post('/auth/logout', logout)
 
 module.exports = router
